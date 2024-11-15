88a9cb95f88c6adad28be5f073fda681fb266185
Shehroz <xiyobidasturchi0011@gmail.com>
2024-11-15 14:30:19 +0500

    Algorotim

---
+++ Algotitm/exam.php
@@ -0,0 +1,42 @@
+<?php
+//$i = 4;
+//while ($i!=15):
+//    $i++;
+//    echo "\n";
+//    echo $i;
+//endwhile;
+//$i = 1;
+//if ($i==0):
+//    echo "Kiritilgan son nol";
+//    endif;
+//if ($i>0):
+//    echo "Kiritilgan son musbat";
+//    endif;
+//
+//if ($i<0):
+//    echo "Kiritilgan son manfiy";
+//    endif;
+//
+//$f = "hello";
+//$d = strlen($f);
+//$w = "";
+//for ($i=$d; $i>=0; $i--):
+//    $w .= $f[$i];
+//endfor;
+//echo $w;
+
+//echo str_word_count('salom qaysan nima gap');
+
+
+//echo strtoupper("nima gapla");
+//$i = 6;
+//$facktorial = 1;
+//for ($j = 1; $j < $i; $j++):
+//    $facktorial *= $j;
+//endfor;
+//echo $facktorial;
+
+
+
+$a = [2,2,3,3,4,4];
+echo array_sum($a);
