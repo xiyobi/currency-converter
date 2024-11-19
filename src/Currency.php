1a4d667f6bad5153726dabccf916de749834e119
Shehroz <xiyobidasturchi0011@gmail.com>
2024-11-19 14:21:13 +0500

    Major

--- src/Currency.php
+++ src/Currency.php
@@ -1,52 +1,61 @@
 <?php
-class Currency{
-
+class Currency
+{
     const Currency_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
-
     public array $currencies = [];
-    public function __construct(){
 
+    public function __construct()
+    {
         $ch = curl_init();
-
         curl_setopt($ch, CURLOPT_URL, self::Currency_API_URL);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $output = curl_exec($ch);
         curl_close($ch);
-        $decoded = json_decode($output);
-        $this->currencies = $decoded;
-        array_unshift($this->currencies, (object)[
-            'Ccy'=>'UZS',
-            'Rate'=>1,
-            'CcyNm_EN'=>'Uzbekistan sum'
-        ]);
 
+        $decoded = json_decode($output);
+        if ($decoded) {
+            $this->currencies = $decoded;
+            array_unshift($this->currencies, (object)[
+                'Ccy' => 'UZS',
+                'Rate' => 1,
+                'CcyNm_EN' => 'Uzbekistan sum'
+            ]);
+        }
     }
-    public function  getCurrencies():array
+
+    public function getCurrencies(): array
     {
         $separated_data = [];
-        $currencies_info = $this->currencies;
-        foreach ($currencies_info as $currency):
-                $separated_data[$currency->Ccy] = $currency->Rate;
-
-           endforeach;
-
-        return array_slice($separated_data, 0, 5);
-
+        foreach ($this->currencies as $currency) {
+            $separated_data[$currency->Ccy] = $currency->Rate;
+        }
+        return array_slice($separated_data, 0, 5); // Barcha valyutalarni qaytarish uchun tuzatildi
     }
-    public function  exchange(string $fromCurrency, string $toCurrency,int $amount)
+
+    public function exchange(string $fromCurrency, string $toCurrency, float $amount)
     {
         $currencies = $this->getCurrencies();
+
+        if (!isset($currencies[$fromCurrency]) || !isset($currencies[$toCurrency])) {
+            return "Valyuta topilmadi.";
+        }
+
         if ($fromCurrency == $toCurrency) {
-            return "valutalar bir xil ";
-        }
-        if ($toCurrency=='UZS') {
-            return $amount*(int)$currencies[$fromCurrency];
-        }
-        if ($fromCurrency=='UZS') {
-            return $amount/(int)$currencies[$fromCurrency];
+            return "Valutalar bir xil.";
         }
 
-            return "O'zbek so'mi kiritilmadi";
+        // UZS dan boshqa valyutaga almashtirish
+        if ($fromCurrency == 'UZS') {
+            return $amount / (float)$currencies[$toCurrency];
+        }
 
+        // Boshqa valyutadan UZSga almashtirish
+        if ($toCurrency == 'UZS') {
+            return $amount * (float)$currencies[$fromCurrency];
+        }
+
+        // Valyutani boshqa valyutaga almashtirish
+        $converted_to_uzs = $amount * (float)$currencies[$fromCurrency];
+        return $converted_to_uzs / (float)$currencies[$toCurrency];
     }
 }
